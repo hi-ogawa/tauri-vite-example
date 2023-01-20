d9ac9045bfee3e28f510dc79bd67f42360a73c91
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-01-20 13:30:01 +0900

    chore: add index-dev.html

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig((env) => ({
+  appType: env.mode === "development" ? "mpa" : "spa",
   plugins: [unocss(), react()],
-});
+}));
