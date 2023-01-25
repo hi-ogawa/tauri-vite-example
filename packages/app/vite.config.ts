42e0ecbcfc1b0759cc100e889619ba772c8211cf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-01-25 12:55:42 +0900

    chore: move vite root

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig((env) => ({
+  root: "./src",
   appType: env.mode === "development" ? "mpa" : "spa",
   plugins: [unocss(), react()],
 }));
